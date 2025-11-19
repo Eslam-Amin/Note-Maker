2e34778210c23eb1f79bdc410189927d36a12986
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-20 01:08:43 +0200

    Created basic main file with the use of noteStruct

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+)
+
+
+func main (){
+	title, content := getNoteData()
+	userNote, err :=note.New(title, content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.DisplayNote()
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note Title:")
+
+	content := getUserInput("Note Content:")
+	return title, content
+}
+
+func getUserInput(prompt string) (string) {
+	fmt.Printf("%v ",prompt)
+	reader := bufio.NewReader(os.Stdin)
+	// '\n' is the newline character where we press enter, and should be read
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+	return text
+}
+
