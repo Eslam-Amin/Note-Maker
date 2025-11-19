828c9a4c233300cb988e388453089422a9d9c55d
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-20 01:34:29 +0200

    Created Todo struct

---
+++ todo/todo.go
@@ -0,0 +1,44 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+
+type Todo struct{
+	Text 	string	`json:"text"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+
+func (todo Todo) DisplayTodo(){
+	fmt.Println(todo)
+}
+
+func (todo Todo) Save()error{
+	fileName := "todo.json"
+	jsonContent, err := json.Marshal(todo)
+	
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, jsonContent, 0644)
+	
+}
+
+
+func New (content string) (*Todo, error){
+	if content == "" {
+		return nil, errors.New("title and content are required")
+	}
+
+	return &Todo{
+		Text: content,
+		CreatedAt: time.Now(),
+	}, nil 
+}
